a485829f9aebdef5c9c7490055c0abc9c08b5446
Tierra <sr.undershaft@gmail.com>
2019-09-11 21:08:19 -0500

    added customer and manager logic

--- assets/javascript/bamazonCustomer.js
+++ assets/javascript/bamazonCustomer.js
@@ -1,11 +1,112 @@
-const MYSQL = require("mysql");
-const INQUIRE = require("inquirer")
+const mysql = require("mysql");
+const inquire = require("inquirer");
+const cTable = require("console.table");
 
-const CON = mysql.createConnection({
+const conn = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "password",
     database: "bamazonDB"
-  });
-  
+});
+
+conn.connect(function (err) {
+    if (err) throw err;
+    initiateStore();
+
+})
+let available_choices = [];
+let available_IDs = [];
+
+function initiateStore() {
+    conn.query("SELECT item_id, product_name, price FROM products WHERE stock_quantity > 0", function (err, res) {
+        if (err) throw err;
+        available_choices = [];
+        available_IDs = [];
+
+
+        for (let i = 0; i < res.length; i++) {
+            let id = res[i].item_id;
+            let name = res[i].product_name;
+            let price = "$" + res[i].price.toFixed(2);
+            let productDetails = { ID: id, Product: name, Price: price };
+            available_choices.push(productDetails);
+            available_IDs.push(productDetails.ID)
+
+        }
+        let displayChoices = cTable.getTable(available_choices);
+        console.log(displayChoices);
+
+        makePurchase(available_choices);
+    })
+};
+
+function makePurchase(stock) {
+    inquire.prompt([{
+        type: "number",
+        name: "purchase",
+        message: "Enter the ID number of the item you wish to purchase.",
+        validate: (input) => {
+            let selection = parseInt(input);
+            return available_IDs.includes(selection) || "That product is not available.  Select another item"
+        }
+    }, {
+        type: "number",
+        name: "amount",
+        message: "Enter the amount you would like to buy",
+        validate: (amount) => {
+            var reg = /^\d+$/;
+            return reg.test(amount) || "Amount must be a number!";
+        },
+        when: function (answer) {
+            if (answer.purchase) {
+                return true
+            } else return false;
+        }
+
+    }]).then(answers => {
+        checkStock(answers.purchase, answers.amount);
+    })
+}
+
+function checkStock(itemID, itemAMT) {
+    conn.query("SELECT * FROM products WHERE item_id=?", itemID, function (err, res) {
+        if (err) {
+            console.log(err)
+        }
+        let stock_AMT = res[0].stock_quantity;
+        let name = res[0].product_name;
+        let price = res[0].price;
+        if (stock_AMT >= itemAMT) {
+            let total = price * itemAMT;
+            console.log(`
+            You have successfully completed a purchase order for ${itemAMT} ${name}(s)
+            Total cost: $ ${total}.
+            Pay on delivery.`)
+            let new_Amt = stock_AMT - itemAMT;
+            conn.query(`UPDATE products SET stock_quantity = ${new_Amt} WHERE item_id = ${itemID}`, function (err, result) {
+                if (err) throw err;
+                // console.log(result.affectedRows + " record(s) updated")
+            })
+            continueShopping();
+        } else {
+            console.log(`Insufficient Quantity.  There are ${stock_AMT} ${name}(s) remaining.`)
+            continueShopping();
+        }
+    })
+};
+
+function continueShopping() {
+    inquire.prompt({
+        type: "expand",
+        name: "continue",
+        message: "Would you like to continue shopping? Enter y(yes) or n(no) to choose",
+        choices: [{name: "Yes, I would like to keep shopping", value: true, key: "Y"}, {name: "No, I have gotten everything I want", value: false, key: "n"}]
+    }).then(answer => {
+        if (answer.continue) {
+            initiateStore();
+        } else {
+            conn.end();
+        }
+    })
+};
---
+++ assets/javascript/bamazonManager.js
@@ -0,0 +1,167 @@
+const mysql = require("mysql");
+const inquire = require("inquirer");
+const cTable = require("console.table");
+
+const conn = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "bamazonDB"
+});
+
+conn.connect(function (err) {
+    if (err) throw err;
+    queryUser();
+
+})
+let userOptions = ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"];
+// * List a set of menu options:
+function queryUser() {
+    inquire.prompt({
+        type: "list",
+        name: "prompt",
+        message: "What would you like to do?",
+        choices: userOptions
+    }).then((answer) => {
+        switch (answer.prompt) {
+            case userOptions[0]:
+                viewProducts();
+                break;
+            case userOptions[1]:
+                viewLowInv();
+                break;
+            case userOptions[2]:
+                addToInv();
+                break;
+            case userOptions[3]:
+                addNewProd();
+                break;
+            default:
+                console.log("Oopsies, something has gone wrong. Please try again later");
+                break;
+        }
+
+    })
+}
+// * View Products for Sale
+function viewProducts() {
+    conn.query("SELECT * FROM products", (err, res) => {
+        if (err) {
+            console.log(err);
+        }
+        let formatted = res.map(product => {
+            return {
+                ID: product.item_id,
+                Product_Name: product.product_name,
+                Department: product.department_name,
+                Price: product.price.toFixed(2),
+                Stock_Quantity: product.stock_quantity
+            }
+        })
+        console.table(formatted);
+
+        conn.end();
+    });
+}
+// * View Low Inventory
+function viewLowInv() {
+    conn.query("SELECT * FROM products WHERE stock_quantity <=?", 5, (err, res) => {
+        if (err) {
+            console.log(err);
+        }
+        console.table(res);
+        conn.end();
+    });
+
+}
+// * Add to Inventory
+function addToInv() {
+    let itemIDs = [];
+    conn.query("SELECT * FROM products", (err, res) => {
+        if (err) {
+            console.log(err);
+        }
+        for (let i = 0; i < res.length; i++) {
+            let id = parseInt(res[i].item_id);
+            itemIDs.push(id);
+        }
+        reorderPrompt(itemIDs).then((answers) => {
+            let itemID = answers.product;
+            let orderAmt = answers.orderAmt;
+            conn.query(`UPDATE products SET stock_quantity = stock_quantity + ${orderAmt} WHERE item_id = ${itemID}`, (err, response) => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log(response.affectedRows + " record(s) updated");
+                conn.end();
+            })
+        });
+    });
+
+}
+// * Add New Product
+function addNewProd() {
+    newProductPrompt().then((answers) => {
+        let productName = answers.productName;
+        let departmentName = answers.departmentName;
+        let productPrice = answers.price;
+        let stockQuant = answers.quantity;
+        conn.query('INSERT INTO products SET ?', {
+            product_name: productName,
+            department_name: departmentName,
+            price: productPrice,
+            stock_quantity: stockQuant
+        }, (err, response) => {
+            if (err) throw err;
+            console.log(response.affectedRows + " record(s) updated")
+            conn.end();
+        })
+    })
+}
+function newProductPrompt() {
+    return inquire.prompt([{
+        type: "input",
+        message: "Enter the new product name.",
+        name: "productName"
+    }, {
+        type: "input",
+        message: "Enter the department of the new product",
+        name: "departmentName"
+    }, {
+        type: "number",
+        message: "Enter the price per unit of the new product",
+        name: "price",
+        validate: (input) => {
+            return !isNaN(input) || "Please enter a valid numeric price"
+        }
+    }, {
+        type: "number",
+        message: "Enter the amount of the product in stock",
+        name: "quantity",
+        validate: (input) => {
+            if (input >= 0) {
+                return Number.isInteger(input) || "Please enter a valid integer"
+            } else {
+                return "Please enter a valid positive integer"
+            }
+        }
+    }])
+};
+
+function reorderPrompt(itemIDs) {
+    return inquire.prompt([{
+        type: "number",
+        message: "Enter the item ID of the product you wish to restock",
+        name: "product",
+        validate: (input) => {
+            return itemIDs.includes(input) || "That is not a valid item ID.  Please re-enter a valid item ID."
+        }
+    }, {
+        type: "list",
+        name: "orderAmt",
+        message: "Choose an amount to reorder.",
+        choices: [{ name: "5", value: 5 }, { name: "10", value: 10 }, { name: "50", value: 50 }, { name: "100", value: 100 }]
+
+    }])
+}
